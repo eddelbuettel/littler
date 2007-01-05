efbe25aa7f3fafd9c1df25705d28c21918c2675c
Dirk Eddelbuettel <edd@debian.org>
2007-01-05 01:44:18 +0000

    release 0.0.9
    git-svn-id: https://littler.googlecode.com/svn/trunk@110 8c87aca1-bb1c-0410-8357-fd783f80b367

--- examples/update.r
+++ examples/update.r
@@ -9,5 +9,10 @@ repos <- "http://cran.r-project.org"
 ## this makes sense on Debian where no package touch /usr/local
 lib.loc <- "/usr/local/lib/R/site-library"
 
+# Always re-generate the available.packages() cache
+cache <- file.path(tempdir(),
+                   paste("repos_", URLencode(repos, TRUE), "*.rds", sep=""))
+unlink(cache)
+
 ## r use requires non-interactive use
 update.packages(repos=repos, ask=FALSE, lib.loc=lib.loc)
