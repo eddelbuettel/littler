e00af3836de652a43778db53145103f376f13a41
Dirk Eddelbuettel <edd@debian.org>
2007-03-21 14:13:38 +0000

    contains the let's-outsmart-the-cache fix by Jeff that somehow never got checked in
    git-svn-id: https://littler.googlecode.com/svn/trunk@118 8c87aca1-bb1c-0410-8357-fd783f80b367

--- examples/update.r
+++ examples/update.r
@@ -5,6 +5,13 @@
 
 ## adjust as necessary, see help('download.packages')
 repos <- "http://cran.r-project.org"  
+#stopifnot(require(methods, quiet=TRUE))        ## S4 methods needed for Biobase
+#repos <- Biobase:::biocReposList()
+
+# Always re-generate the available.packages() cache
+cache <- file.path(tempdir(),
+                   paste("repos_", URLencode(repos, TRUE), "*.rds", sep=""))
+unlink(cache)
 
 ## this makes sense on Debian where no package touch /usr/local
 lib.loc <- "/usr/local/lib/R/site-library"
