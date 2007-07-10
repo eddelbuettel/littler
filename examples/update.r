cc032c679ba9c285a5cbcec6326daccd03a26128
Dirk Eddelbuettel <edd@debian.org>
2007-07-10 02:17:19 +0000

    fixes to dealing with caches in tempdir /tmp
    git-svn-id: https://littler.googlecode.com/svn/trunk@124 8c87aca1-bb1c-0410-8357-fd783f80b367

--- examples/update.r
+++ examples/update.r
@@ -5,21 +5,35 @@
 
 ## adjust as necessary, see help('download.packages')
 repos <- "http://cran.r-project.org"  
-#stopifnot(require(methods, quiet=TRUE))        ## S4 methods needed for Biobase
-#repos <- Biobase:::biocReposList()
-
-# Always re-generate the available.packages() cache
-cache <- file.path(tempdir(),
-                   paste("repos_", URLencode(repos, TRUE), "*.rds", sep=""))
-unlink(cache)
 
 ## this makes sense on Debian where no package touch /usr/local
 lib.loc <- "/usr/local/lib/R/site-library"
 
-# Always re-generate the available.packages() cache
-cache <- file.path(tempdir(),
-                   paste("repos_", URLencode(repos, TRUE), "*.rds", sep=""))
-unlink(cache)
+# Always clear available.packages() cache of remote and local files
+clearCache <- function(repos, lib.loc) {
+  remotecf <- file.path(tempdir(),
+                        paste("repos_", URLencode(contrib.url(repos), TRUE),
+                              ".rds", sep=""))
+  if (file.exists(remotecf)) {
+    #cat("Removing remote cache file\n")
+    unlink(remotecf)
+  }
+  localcf <- file.path(tempdir(),
+                       paste("libloc_", URLencode(lib.loc, TRUE),
+                             "Version,Priority,Bundle,Contains,",
+                             "Depends,Imports,Suggests,Built.rds",
+                             sep=""))
+  if (file.exists(localcf)) {
+    ##cat("Removing local cache file\n")
+    unlink(localcf)
+  }
+}
+
+## Always clear caches of remote and local packages
+clearCache(repos, lib.loc)
 
 ## r use requires non-interactive use
 update.packages(repos=repos, ask=FALSE, lib.loc=lib.loc)
+
+## Always clear caches of remote and local packages
+clearCache(repos, lib.loc)
