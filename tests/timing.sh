5573d9bb2fa94069aee2877ff88e92299da3996e
Dirk Eddelbuettel <edd@debian.org>
2008-01-15 02:57:04 +0000

    added Rscript tests; re-indented to emacs standard for sh scripts
    git-svn-id: https://littler.googlecode.com/svn/trunk@137 8c87aca1-bb1c-0410-8357-fd783f80b367

--- tests/timing.sh
+++ tests/timing.sh
@@ -2,38 +2,50 @@
 
 N=10
 function loopBC {
-  echo 
-  echo -n " --- GNU bc doing the addition $N times"
-  i=0
-  while [ $i -lt $N ]
-  do
-    echo "1+2" | bc -l > /dev/null
-    i=$((i + 1))
-  done
+    echo 
+    echo -n " --- GNU bc doing the addition $N times"
+    i=0
+    while [ $i -lt $N ]
+    do
+	echo "1+2" | bc -l > /dev/null
+	i=$((i + 1))
+    done
 }
 
 function loopLittleR {
-  echo
-  echo -n " --- our r doing the addition $N times"
-  i=0
-  while [ $i -lt $N ]
-  do
-    echo "cat(1+2,'\n')" | ../r --vanilla > /dev/null
-    i=$((i + 1))
-  done
+    echo
+    echo -n " --- our r doing the addition $N times"
+    i=0
+    while [ $i -lt $N ]
+    do
+	echo "cat(1+2,'\n')" | ../r --vanilla > /dev/null
+	i=$((i + 1))
+    done
+}
+
+function loopRscript {
+    echo
+    echo -n " --- GNU R's Rscript doing the addition $N times"
+    i=0
+    while [ $i -lt $N ]
+    do
+	echo "cat(1+2,'\n')" | Rscript - > /dev/null
+	i=$((i + 1))
+    done
 }
 
 function loopR {
-  echo
-  echo -n " --- the real GNU R doing the addition $N times"
-  i=0
-  while [ $i -lt $N ]
-  do
-    echo "cat(1+2,'\n')" | R --slave --vanilla > /dev/null
-    i=$((i + 1))
-  done
+    echo
+    echo -n " --- GNU R doing the addition $N times"
+    i=0
+    while [ $i -lt $N ]
+    do
+	echo "cat(1+2,'\n')" | R --slave --vanilla > /dev/null
+	i=$((i + 1))
+    done
 }
 
 time loopBC
 time loopLittleR
+time loopRscript
 time loopR
--- tests/timing2.sh
+++ tests/timing2.sh
@@ -3,26 +3,38 @@
 N=20
 
 function loopLittleR {
-  echo
-  echo -n " --- our r calling summary() $N times"
-  i=0
-  while [ $i -lt $N ]
-  do
-    ./summary2.R >/dev/null
-    i=$((i + 1))
-  done
+    echo
+    echo -n " --- our r calling summary() $N times"
+    i=0
+    while [ $i -lt $N ]
+    do
+	./summary2.R >/dev/null
+	i=$((i + 1))
+    done
+}
+
+function loopRscript {
+    echo
+    echo -n " --- GNU R's Rscript calling summary() $N times"
+    i=0
+    while [ $i -lt $N ]
+    do
+	Rscript ./summary2.R >/dev/null
+	i=$((i + 1))
+    done
 }
 
 function loopR {
-  echo
-  echo -n " --- the real GNU R calling summary() $N times"
-  i=0
-  while [ $i -lt $N ]
-  do
+    echo
+    echo -n " --- GNU R calling summary() $N times"
+    i=0
+    while [ $i -lt $N ]
+    do
 	R --silent --vanilla < ./summary2.R >/dev/null
-    i=$((i + 1))
-  done
+	i=$((i + 1))
+    done
 }
 
 time loopLittleR
+time loopRscript
 time loopR
