a251546a8e79d5377633503d48bc8d9cf5f5e59e
Dirk Eddelbuettel <edd@debian.org>
2008-03-12 16:52:29 +0000

    simpler clearCache, may need a layer of try()
    git-svn-id: https://littler.googlecode.com/svn/trunk@140 8c87aca1-bb1c-0410-8357-fd783f80b367

--- examples/update.r
+++ examples/update.r
@@ -14,31 +14,16 @@ if (rc) {
 ## this makes sense on Debian where no package touch /usr/local
 lib.loc <- "/usr/local/lib/R/site-library"
 
-# Always clear available.packages() cache of remote and local files
-clearCache <- function(repos, lib.loc) {
-  remotecf <- file.path(tempdir(),
-                        paste("repos_", URLencode(contrib.url(repos), TRUE),
-                              ".rds", sep=""))
-  if (file.exists(remotecf)) {
-    #cat("Removing remote cache file\n")
-    unlink(remotecf)
-  }
-  localcf <- file.path(tempdir(),
-                       paste("libloc_", URLencode(lib.loc, TRUE),
-                             "Version,Priority,Bundle,Contains,",
-                             "Depends,Imports,Suggests,Built.rds",
-                             sep=""))
-  if (file.exists(localcf)) {
-    ##cat("Removing local cache file\n")
-    unlink(localcf)
-  }
+## simply unrolling of all unlink over all files 'repos*' in $TMP
+clearCache <- function() {
+  sapply(list.files(path=tempdir(), pattern="repos*", full.names=TRUE), unlink)
 }
 
 ## Always clear caches of remote and local packages
-clearCache(repos, lib.loc)
+clearCache()
 
 ## r use requires non-interactive use
 update.packages(repos=repos, ask=FALSE, lib.loc=lib.loc)
 
 ## Always clear caches of remote and local packages
-clearCache(repos, lib.loc)
+clearCache()
