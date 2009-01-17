c4419f8333c5b81ab2685c0e447599ce87b266b9
Dirk Eddelbuettel <edd@debian.org>
2009-01-17 21:11:25 +0000

    g++ now prefers const char*, so may as well create const char array
    git-svn-id: https://littler.googlecode.com/svn/trunk@167 8c87aca1-bb1c-0410-8357-fd783f80b367

--- littler.R
+++ littler.R
@@ -1,7 +1,7 @@
 ExcludeVars <- c("R_SESSION_TMPDIR","R_HISTFILE")
 IncludeVars <- Sys.getenv()
 IncludeVars <- IncludeVars[grep("^R_",names(IncludeVars),perl=TRUE)]
-cat("char *R_VARS[] = {\n")
+cat("const char *R_VARS[] = {\n")
 for (i in 1:length(IncludeVars)){
 	if (names(IncludeVars)[i] %in% ExcludeVars)
 		next
