07c8ee2052b065706bd4ae9fe44538a7ab503bcb
Dirk Eddelbuettel <edd@debian.org>
2010-01-06 02:57:41 +0000

    final (?) commit for 0.1.3 release -- improvement to ldflags.R so that no 'empty' rpath entry gets created which had upset littler
    git-svn-id: https://littler.googlecode.com/svn/trunk@178 8c87aca1-bb1c-0410-8357-fd783f80b367

--- ldflags.R
+++ ldflags.R
@@ -1,6 +1,10 @@
+
 s <- Sys.info()
+if (is.null(s))
+    q()
 
-if (is.null(s)) q()
-
-if (s[1] == "Linux")
-	cat(paste('-Wl,-rpath,',strsplit(Sys.getenv('LD_LIBRARY_PATH'),':')$LD_LIBRARY_PATH,sep=''),"\n",sep=' ')
+if (s[1] == "Linux") {
+    ldp <- strsplit(Sys.getenv('LD_LIBRARY_PATH'),':')$LD_LIBRARY_PATH
+    ldp <- ldp[which(ldp != "")]
+    cat(paste('-Wl,-rpath,',ldp,sep=''),"\n",sep=' ')
+}
