ca8853e864988cb70146dd911d400d62a913e689
Dirk Eddelbuettel <edd@debian.org>
2014-07-13 09:51:04 -0500

    added compAttr.r

---
+++ examples/compAttr.r
@@ -0,0 +1,17 @@
+#!/usr/bin/r
+#
+# Simple helper script for compileAttributes() 
+#
+# Dirk Eddelbuettel, July 2014
+#
+# GPL-2 or later
+
+## load Rcpp
+suppressMessages(library(Rcpp))
+
+## check all command-line arguments (if any are given) for directory status
+argv <- Filter(function(x) file.info(x)$is.dir, argv)
+
+## loop over all argument, with fallback of the current directory, and
+## call compileAttributes() on the given directory
+sapply(ifelse(length(argv) > 0, argv, "."), compileAttributes)
