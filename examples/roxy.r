9fe57dddb3c973bbc67316faa46cbcfaf972f105
Dirk Eddelbuettel <edd@debian.org>
2014-07-14 06:00:56 -0500

    expanded roxy.r example

--- examples/roxy.r
+++ examples/roxy.r
@@ -1,3 +1,17 @@
 #!/usr/bin/r
-suppressMessages(library(roxygen2))
-roxygenize(".", roclets="rd")
+#
+# Simple helper script for roxygen2::roxyegnize() 
+#
+# Dirk Eddelbuettel, August 2013
+#
+# GPL-2 or later
+
+## load roxygen
+library(roxygen2)
+
+## check all command-line arguments (if any are given) for directory status
+argv <- Filter(function(x) file.info(x)$is.dir, argv)
+
+## loop over all argument, with fallback of the current directory, and
+## call compileAttributes() on the given directory
+sapply(ifelse(length(argv) > 0, argv, "."), FUN=roxygenize, roclets="rd")
