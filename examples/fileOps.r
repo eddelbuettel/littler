8da72ef5cd5b862286eb7e3c2c744c6724075005
Dirk Eddelbuettel <edd@debian.org>
2014-08-31 17:54:00 -0500

    some tweaks to fileOps example

--- examples/fileOps.r
+++ examples/fileOps.r
@@ -1,13 +1,23 @@
 #!/usr/bin/env r
 
 ## read files
-files <- dir("/some/directory", full.names=TRUE)
+files <- dir("..", full.names=TRUE)
 
 ## creation time as Date
 ctimes <- as.Date(file.info(files)$ctime)
 
-## select based on time window
-ind <- ctimes >= as.Date("2013-01-01") & ctimes <= as.Date("2013-01-31")
+## select based on time window -- something arbitrary
+now <- Sys.Date()
+## these are appromiximative, could be refined using DateTimeClasses
+sixmon <- now - 365.25/2
+twelvemon <- now - 365.2
+
+ind <- ctimes >= twelvemon & ctimes <= sixmon
+
+if (sum(ind) > 0) {
+    cat("The following files are between six and twelve months old\n")
+    print(files[ind])
+}
 
 ## do something
 ##   print(files[ind])
