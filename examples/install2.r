7fcd06d01b492afce887b47e7a1cb4bb15765a2f
Dirk Eddelbuettel <edd@debian.org>
2014-09-29 19:41:47 -0500

    minor tweaks to install2.r and installGitHub.r

--- examples/install2.r
+++ examples/install2.r
@@ -2,16 +2,16 @@
 #
 # a simple example to install one or more packages
 
-## load docopt package from CRAN, or stop if not available
-suppressMessages(library(docopt))       # we need the docopt package 0.3 or later
+## load docopt package from CRAN
+suppressMessages(library(docopt))       # we need docopt (>= 0.3) as on CRAN
 
 ## configuration for docopt
 doc <- "Usage: install.r [-r REPO] [-l LIBLOC] [-h] [-d DEPS] [PACKAGES ...]
 
 -r --repos REPO     repository to install from [default: http://cran.rstudio.com]
 -l --libloc LIBLOC  location in which to install [default: /usr/local/lib/R/site-library]
--h --help                   show this help text
--d --deps DEPS      Install suggested dependencies as well? [default: FALSE]"
+-d --deps DEPS      Install suggested dependencies as well? [default: FALSE]
+-h --help           show this help text"
 
 ## docopt parsing
 opt <- docopt(doc)
@@ -21,6 +21,3 @@ install.packages(pkgs  = opt$PACKAGES,
                  lib   = opt$libloc,
                  repos = opt$repos,
                  dependencies=opt$deps)
-
-# Let errors be errors
-# q(status=0)
