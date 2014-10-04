d1abc25bbae265e1c271c3851e87f756cd2ed94d
Carl Boettiger <cboettig@gmail.com>
2014-10-03 20:02:08 -0700

    add docopt-based test.r function

---
+++ examples/test.r
@@ -0,0 +1,28 @@
+#!/usr/bin/env r
+#
+# a simple example to install one or more packages
+
+## load docopt package from CRAN, or stop if not available
+suppressMessages(library(docopt))       # we need the docopt package 0.3 or later
+suppressMessages(library(tools))      
+
+## configuration for docopt
+doc <- "Usage: test.r [-l LIBLOC] [-o OUTDIR] [-t TYPES] [-s SRCDIR] [-h] [PACKAGES ...]
+
+-l --libloc LIBLOC  location where package(s) are installed [default: NULL]
+-o --outdir OUTDIR  the directory into which to write the output files: this should already exist. [default: .]
+-t --types TYPES    type(s) of tests to be done. [default: c('examples', 'tests', 'vignettes')]
+-s --srcdir SRCDIR  Optional directory to look for .save files [default: NULL]
+-h --help                   show this help text"
+
+
+## docopt parsing
+opt <- docopt(doc)
+
+## installation given selected options and arguments
+testInstalledPackage(pkg = opt$PACKAGES,
+                     lib = opt$libloc,
+                  outDir = opt$outdir,
+                   types = opt$types,
+                  srcdir = opt$srcdir)
+
