7d6ba8a4dee48591ef6f5380c5d2c15dbafa8b92
Dirk Eddelbuettel <edd@debian.org>
2014-10-04 16:16:30 -0500

    renamed test.r to testInstalled.r, minor edits, changelog entry

--- examples/testInstalled.r
+++ examples/testInstalled.r
@@ -1,6 +1,6 @@
 #!/usr/bin/env r
 #
-# a simple example to install one or more packages
+# a simple example to test one or more installed packages
 
 ## load docopt package from CRAN, or stop if not available
 suppressMessages(library(docopt))       # we need the docopt package 0.3 or later
@@ -10,10 +10,10 @@ suppressMessages(library(tools))
 doc <- "Usage: test.r [-l LIBLOC] [-o OUTDIR] [-t TYPES] [-s SRCDIR] [-h] [PACKAGES ...]
 
 -l --libloc LIBLOC  location where package(s) are installed [default: NULL]
--o --outdir OUTDIR  the directory into which to write the output files: this should already exist. [default: .]
+-o --outdir OUTDIR  the directory into which to write output files, should exist. [default: .]
 -t --types TYPES    type(s) of tests to be done. [default: c('examples', 'tests', 'vignettes')]
 -s --srcdir SRCDIR  Optional directory to look for .save files [default: NULL]
--h --help                   show this help text"
+-h --help           show this help text"
 
 
 ## docopt parsing
