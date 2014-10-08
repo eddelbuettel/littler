1f91a2abf434442ee4db67a050091175eeaff358
Carl Boettiger <cboettig@gmail.com>
2014-10-08 16:11:08 -0700

    Treat the --deps arg as a boolean, not a string
    The --deps argument was being treated as a string, causing
    the default 'FALSE' to thow an error.  It is now treated
    as a boolean, like the --error toggle.
    
    The R function install.packages allows deps to be either
    a logical/boolean value or a string with any combination of
    the elements SUGGESTS, IMPORTS, DEPENDS, etc, so this
    change limits the functionality.  Trying to handle both
    cases with command line arguments would probably make the
    code too messy, so I just stuck with the boolean-only strategy.

--- examples/install2.r
+++ examples/install2.r
@@ -6,11 +6,11 @@
 suppressMessages(library(docopt))       # we need docopt (>= 0.3) as on CRAN
 
 ## configuration for docopt
-doc <- "Usage: install.r [-r REPO] [-l LIBLOC] [-h] [-d DEPS] [--error] [PACKAGES ...]
+doc <- "Usage: install.r [-r REPO] [-l LIBLOC] [-h] [--deps] [--error] [PACKAGES ...]
 
 -r --repos REPO     repository to install from [default: http://cran.rstudio.com]
 -l --libloc LIBLOC  location in which to install [default: /usr/local/lib/R/site-library]
--d --deps DEPS      Install suggested dependencies as well? [default: FALSE]
+-d --deps           Install suggested dependencies as well? [default: FALSE]
 -e --error          Throw error and halt if package cannot be installed? (usually a warning). [default: FALSE]
 -h --help           show this help text"
 
