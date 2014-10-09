f39a9ba86491c46e36de2d7800c6f02808b344f4
Carl Boettiger <cboettig@gmail.com>
2014-10-08 21:58:07 -0700

    Ugh, another bug in install2.r deps The default argument should be NA, not 'FALSE'.  Consequently, we need to do the type error handling to coerce logicals strigns to logicals and NA strings to NA.  On the down side this means we use the inconsistent-looking notation to pass the value --deps TRUE while passing error as a boolean with the flag alone (--error).  On the upside we can also pass strings like c('IMPORTS', 'DEPENDS').

--- examples/install2.r
+++ examples/install2.r
@@ -6,16 +6,21 @@
 suppressMessages(library(docopt))       # we need docopt (>= 0.3) as on CRAN
 
 ## configuration for docopt
-doc <- "Usage: install.r [-r REPO] [-l LIBLOC] [-h] [--deps] [--error] [PACKAGES ...]
+doc <- "Usage: install.r [-r REPO] [-l LIBLOC] [-h] [-d DEPS] [--error] [PACKAGES ...]
 
 -r --repos REPO     repository to install from [default: http://cran.rstudio.com]
 -l --libloc LIBLOC  location in which to install [default: /usr/local/lib/R/site-library]
--d --deps           Install suggested dependencies as well? [default: FALSE]
+-d --deps DEPS      Install suggested dependencies as well [default: NA]
 -e --error          Throw error and halt if package cannot be installed? (usually a warning). [default: FALSE]
 -h --help           show this help text"
 
 ## docopt parsing
 opt <- docopt(doc)
+
+if(opt$deps == "TRUE" || opt$deps == "FALSE")
+  opt$deps <- as.logical(opt$deps)
+if(opt$deps == "NA")
+  opt$deps <- NA
 
 ## installation given selected options and arguments
 
