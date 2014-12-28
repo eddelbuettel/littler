9f4d0e67348a0e5cef0218c55920a24e9996dd56
Dirk Eddelbuettel <edd@debian.org>
2014-12-28 09:50:14 -0600

    install.r is now smarter about its arguments and detects locally installable files

--- examples/install.r
+++ examples/install.r
@@ -10,21 +10,28 @@
 # Released under GPL (>= 2)
 
 if (is.null(argv) | length(argv) < 1) {
-    cat("Usage: installr.r pkg1 [pkg2 pkg3 ...]\n")
+    cat("Usage: installr.r pkg1 [pkg2 pkg3 ...]\n\n")
     cat("Set environment variables REPOS and LIBLOC to overrride defaults.\n")
-    cat("Use install2.r for a version which supports command-line parsing.\n")
+    cat("Installs pkg1, ... from existing files with matching extension.\n")
     q()
 }
 
 ## adjust as necessary, see help('download.packages')
 repos <- Sys.getenv("REPOS", unset="http://cran.rstudio.com")
+## NULL means install file
 if (repos == "NULL") repos = NULL
 
 ## this makes sense on Debian where no packages touch /usr/local
 lib.loc <- Sys.getenv("LIBLOC", unset="/usr/local/lib/R/site-library")
 
-## use argv, lib.loc and repos to install the packages
-install.packages(argv, lib.loc, repos)
+## helper function to for existing files with matching extension
+isMatchingFile <- function(f) file.exists(f) && grepl("(\\.tar\\.gz|\\.tgz|\\.zip)$", f)
+
+## helper function which switches to local (ie NULL) repo if matching file is presented
+installArg <- function(f, lib, rep) install.packages(f, lib, if (isMatchingFile(f)) NULL else repos)
+
+## use argv [filtered by installArg()], lib.loc and repos to install the packages
+sapply(argv, installArg, lib.loc, repos)
 
 ## clean up any temp file containing CRAN directory information
 sapply(list.files(path=tempdir(), pattern="^(repos|libloc).*\\.rds$", full.names=TRUE), unlink)
