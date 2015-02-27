ba9ebebe32a64111d3003fe9ee8e1636ad346257
Dirk Eddelbuettel <edd@debian.org>
2015-02-26 20:14:38 -0600

    improve / correct check.r with very helpful feedback from Stefan Widgren

--- examples/check.r
+++ examples/check.r
@@ -10,14 +10,15 @@
 suppressMessages(library(docopt))       # we need docopt (>= 0.3) as on CRAN
 
 ## configuration for docopt
-doc <- "Usage: check.r [-h] [--as-cran] [--repo REPO] [--install-deps] [--debinst] [--library LIB] [--setwd DIR] [TARGZ ...]
+doc <- "Usage: check.r [-h] [--as-cran] [--repo REPO] [--install-deps] [--deb-pkgs PKGS...] [--use-sudo] [--library LIB] [--setwd DIR] [TARGZ ...]
 
 -a --as-cran        customization similar to CRAN's incoming [default: FALSE]
 -r --repo REPO      repository to use, or NULL for file [default: http://cran.rstudio.com]
 -i --install-deps   also install packages along with their dependencies [default: FALSE]
--d --deb-pkgs PKGS  also install binary .deb packages with their dependencies [default: FALSE]
 -l --library LIB    when installing use this library [default: /usr/local/lib/R/site-library]
 -s --setwd DIR      change to this directoru before undertaking the test [default: ]
+-d --deb-pkgs PKGS  also install binary .deb packages with their dependencies [default: FALSE]
+-u --use-sudo       use sudo when installing .deb packages [default: TRUE]
 -h --help           show this help text"
 
 ## docopt parsing
@@ -48,8 +49,9 @@ installArg <- function(p, lib, rep) {
 }
 
 ## if binary .deb files are to be installed first:
-if (opt$`deb-pkgs`) {
-    cmd <- paste("apt-get install", opt$PKGS)
+if (length(opt$`deb-pkgs`) > 1 || opt$`deb-pkgs` != FALSE) {
+    cmd <- paste0(if (opt$`use-sudo`) "sudo " else "",
+                 "apt-get install -y ", paste(opt$`deb-pkgs`, collapse=" "))
     system(cmd)
 }
 
