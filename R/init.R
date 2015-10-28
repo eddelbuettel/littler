50941f7ab8cb00cb3742989ce17d95f7e1054e99
Dirk Eddelbuettel <edd@debian.org>
2015-10-27 22:23:02 -0500

    more small edits to man page and DESCRIPTION

--- R/init.R
+++ R/init.R
@@ -1,11 +1,19 @@
 
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("The littler package provides 'r' as a binary.")
-    if (unname(Sys.info()["sysname"]) == "Linux") {
+    if (Sys.info()[["sysname"]] == "Linux") {
         if (unname(Sys.which("r")) == "") {
             packageStartupMessage("You could link to the 'r' binary installed in\n'",
                                   system.file("bin", "r", package="littler"),
                                   "' from '/usr/local/bin' in order to use 'r' for scripting.")
+        }
+    }
+    if (Sys.info()[["sysname"]] == "Darwin") {
+        packageStartupMessage("On OS X, 'r' and 'R' are the same so 'lr' is an alternate name for littler.")
+        if (unname(Sys.which("lr")) == "") {
+            packageStartupMessage("You could link to the 'r' binary installed in\n'",
+                                  system.file("bin", "r", package="littler"),
+                                  "' as '/usr/local/bin/lr' in order to use 'lr' for scripting.")
         }
     }
 }
