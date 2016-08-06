3519caf0f51e5ded7ed2298ac29a87f2d14d3894
Dirk Eddelbuettel <edd@debian.org>
2016-08-06 10:25:22 -0500

    new exported function r() (closes #40)

--- R/test.R
+++ R/test.R
@@ -1,5 +1,22 @@
+## Internal, not exported, function used by unit tests
 test <- function(src = "cat(2 + 2)") {
-    r <- file.path(system.file(package="littler"), "bin",
+    system2(r(), c("-e", shQuote(src)), stdout = TRUE, stderr = TRUE)
+}
+
+
+##' Return the path of the install \code{r} binary.
+##'
+##' The test for Windows is of course superfluous as we have no binary for Windows.
+##' Maybe one day...
+##' @title Return Path to \code{r} Binary
+##' @param usecat Optional toggle to request output to stdout (useful in Makefiles)
+##' @return The path is returned as character variable. If the \code{usecat} option is
+##' set the character variable is displayed via \code{\link{cat}} instead.
+##' @author Dirk Eddelbuettel
+r <- function(usecat=FALSE) {
+    p <- file.path(system.file(package="littler"),
+                   "bin",
                    paste0("r", ifelse(.Platform$OS.type=="windows", ".exe", "")))
-    system2(r, c("-e", shQuote(src)), stdout = TRUE, stderr = TRUE)
+    if (usecat) return(cat(p))
+    p
 }
