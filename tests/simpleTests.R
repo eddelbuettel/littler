190bc9ff319a0f441d48c1d31313ee5ec0313d6e
Dirk Eddelbuettel <edd@debian.org>
2016-08-06 21:06:48 -0500

    make simple tests conditional on Linux

--- tests/simpleTests.R
+++ tests/simpleTests.R
@@ -1,3 +1,10 @@
-library(littler)
-stopifnot(identical(littler:::test(), "4"))   # default test is cat(2+2)
-stopifnot(as.numeric(littler:::test('cat(R.version$year)')) >= 2015)  
+
+## These tests were suggested by a user on OS X, and then promptly
+## failed on OS X per CRAN -- so we now make this conditional on Linux
+## where it is known to work
+
+if (unname(Sys.info()["sysname"]) == "Linux") {
+    library(littler)
+    stopifnot(identical(littler:::test(), "4"))   # default test is cat(2+2)
+    stopifnot(as.numeric(littler:::test('cat(R.version$year)')) >= 2015)  
+}
