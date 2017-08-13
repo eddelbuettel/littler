b926e730b499cedae3902e4a4a12a80e35c93943
Dirk Eddelbuettel <edd@debian.org>
2017-08-13 10:18:15 -0500

    updated docopt usage

--- inst/examples/runit.r
+++ inst/examples/runit.r
@@ -2,25 +2,26 @@
 #
 # A simple example to invoke unit tests
 #
-# Copyright (C) 2014  Dirk Eddelbuettel
+# Copyright (C) 2014 - 2017  Dirk Eddelbuettel
 #
 # Released under GPL (>= 2)
 
-suppressMessages(library(docopt))       # we need the docopt package
-suppressMessages(library(RUnit))        # we need the RUnit package
+suppressMessages({
+    library(docopt)       # we need the docopt package
+    library(RUnit)        # we need the RUnit package
+})
 
 doc <- "Usage: runit.r [-p PACKAGES] [--help] [FILES ...]
 
 -p --packages PACKAGES      comma-separated list of packages to install [default: ]
 -h --help                   show this help text"
 
-opt <- docopt(doc, args=argv)
+opt <- docopt(doc)
 
-
-for (p in strsplit(opt[["--packages"]], ",")[[1]])
+for (p in strsplit(opt$packages, ",")[[1]])
     suppressMessages(require(p, character.only=TRUE))
 
-for (f in opt[["FILES"]])
+for (f in opt$FILES)
     runTestFile(f)
 
 q(status=0)
