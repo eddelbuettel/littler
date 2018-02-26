4c85d2816c7d3967eef9c675c7b2b1a3dd827bbb
Dirk Eddelbuettel <edd@debian.org>
2018-02-26 16:19:21 -0600

    update getRStudioDesktop.r helper

--- inst/examples/getRStudioDesktop.r
+++ inst/examples/getRStudioDesktop.r
@@ -14,10 +14,12 @@ suppressMessages(library(rvest))
 
 setwd("/tmp")                           # go to /tmp
 
-url <- "https://dailies.rstudio.com/rstudio/oss/xenial/amd64/"
+#url <- "https://dailies.rstudio.com/rstudio/oss/xenial/amd64/"
+url <- "https://dailies.rstudio.com/rstudio/oss/ubuntu/amd64/"
 pg <- read_html(url)
 tb <- html_table(html_nodes(pg, "table"))[[1]]
 file <- tb[1,1]
-s3url <- "https://s3.amazonaws.com/rstudio-ide-build/desktop/xenial/amd64/"
+#s3url <- "https://s3.amazonaws.com/rstudio-ide-build/desktop/xenial/amd64/"
+s3url <- "https://s3.amazonaws.com/rstudio-ide-build/desktop/trusty/amd64/"
 fileurl <- paste0(s3url, file)
 download.file(fileurl, file, method="wget")
