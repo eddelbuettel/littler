3bd27d4c2d4d8a3a62b9e9d512e46634a5aa69c5
Dirk Eddelbuettel <edd@debian.org>
2018-09-23 07:46:39 -0500

    simple wrapper for rchk calling check(platform="ubuntu-rchk")

---
+++ inst/examples/rchk.r
@@ -0,0 +1,9 @@
+#!/usr/bin/env r
+#
+# Minimal rhub::check() wrapper using rchk()
+#
+# Copyright (C) 2016 - 2018  Dirk Eddelbuettel
+#
+# Released under GPL (>= 2)
+
+rhub::check(".", "ubuntu-rchk")
