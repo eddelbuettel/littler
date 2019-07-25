e9847997d56d7b5ce881f030a097c17d8cc8d63a
Dirk Eddelbuettel <edd@debian.org>
2019-07-24 20:03:29 -0500

    new helper for rhub check on solaris

---
+++ inst/examples/cos.r
@@ -0,0 +1,9 @@
+#!/usr/bin/env r
+#
+# Minimal rhub(".", platform="solaris-x86-patched")
+#
+# Copyright (C) 2016 - 2019  Dirk Eddelbuettel
+#
+# Released under GPL (>= 2)
+
+rhub::check(path=".", platform="solaris-x86-patched")
