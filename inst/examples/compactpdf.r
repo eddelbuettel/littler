77ae3d6fd2cbf2cc2a69f162b8ea06fb8f7780d0
Dirk Eddelbuettel <edd@debian.org>
2019-08-25 08:35:08 -0500

    add simple 'compactpdf.r' helper

---
+++ inst/examples/compactpdf.r
@@ -0,0 +1,34 @@
+#!/usr/bin/env r
+#
+# A simple wrapper for tools::compactPDF
+#
+# Copyright (C) 2019  Dirk Eddelbuettel
+#
+# Released under GPL (>= 2)
+
+## load docopt package from CRAN
+library(docopt)
+
+## configuration for docopt
+doc <- "Usage: compactpdf.r [-h] [-x] [PATH...]
+
+-h --help            show this help text
+-x --usage           show help and short example usage"
+
+opt <- docopt(doc)			# docopt parsing
+
+if (opt$usage) {
+    cat(doc, "\n\n")
+    cat("Examples:
+  compactpdf.r                        # compact in current directory
+
+compactpdf.r is part of littler which brings 'r' to the command-line.
+See http://dirk.eddelbuettel.com/code/littler.html for more information.\n")
+    q("no")
+}
+
+if (length(opt$PATH) == 0) {
+    opt$PATH <- "."
+}
+
+sapply(opt$PATH, function(p) tools::compactPDF(p, gs_quality="ebook"))
