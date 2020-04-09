945b534892afde7bb851b92c1c3f5dc9a95a544e
Sergio Oller <sergioller@gmail.com>
2020-04-09 11:43:40 +0200

    Error as well if dependency is not available

--- inst/examples/install2.r
+++ inst/examples/install2.r
@@ -77,7 +77,7 @@ install_packages2 <- function(pkgs, ..., error = FALSE, skipinstalled = FALSE) {
         if (error) {
             catch <-
                 grepl("download of package .* failed", e$message) ||
-                grepl("package.*(is|are) not available", e$message) ||
+                grepl("(dependenc|package).*(is|are) not available", e$message) ||
                 grepl("installation of package.*had non-zero exit status", e$message)
             if (catch) {
                 e <<- e
