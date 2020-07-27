66b6a53fedbacff6bba2cd6e5c76b69f5f39f688
Dirk Eddelbuettel <edd@debian.org>
2020-07-26 19:48:48 -0500

    also support command-line argument for checking

--- inst/examples/cos.r
+++ inst/examples/cos.r
@@ -2,8 +2,12 @@
 #
 # Minimal rhub(".", platform="solaris-x86-patched")
 #
-# Copyright (C) 2016 - 2019  Dirk Eddelbuettel
+# Copyright (C) 2016 - 2020  Dirk Eddelbuettel
 #
 # Released under GPL (>= 2)
 
-rhub::check(path=".", platform="solaris-x86-patched")
+checkargs <- "--no-manual --no-vignettes"
+solaris <- "solaris-x86-patched"
+checkfile <- function(f) if (file.exists(f)) rhub::check(f, platform=solaris, check_args=checkargs)
+if (length(argv) == 0) argv <- "."
+sapply(argv, checkfile)
