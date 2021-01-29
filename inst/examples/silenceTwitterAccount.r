4cbd7150da97c677367f5f2d44e096cc37209aa9
Dirk Eddelbuettel <edd@debian.org>
2021-01-29 17:47:58 -0600

    add wrapper for rtweet extension so silence account

---
+++ inst/examples/silenceTwitterAccount.r
@@ -0,0 +1,17 @@
+#!/usr/bin/r
+
+if (is.null(argv) | length(argv) < 1) {
+    cat("Usage: silenceTwitterAccount.r user1 [user2 user3 ...]\n\n")
+    cat("Calls rtweet::post_silence() for the given users.\n")
+    cat("NB: Needs a modified version of rtweet and a twitter token.\n\n")
+    q()
+}
+
+if (!requireNamespace("rtweet", quietly=TRUE))
+    stop("Please install `rtweet`.", call. = FALSE)
+
+if (Sys.getenv("TWITTER_PAT") == "")
+    stop("Please setup a 'TWITTER_PAT' using `rtweet`.", call. = FALSE)
+
+for (arg in argv)
+    rtweet::post_silence(arg)
