adc96abefb7e1eb68729916c16229d988d1aeae7
Dirk Eddelbuettel <edd@debian.org>
2021-03-12 20:03:41 -0600

    expand kitten to call Rcpp*.package.skeleton variants

--- inst/examples/kitten.r
+++ inst/examples/kitten.r
@@ -3,12 +3,13 @@
 library(docopt)
 
 ## configuration for docopt
-doc <- "Usage: kitten.r [-b] [-p] [-h] [-x] PACKAGE
+doc <- "Usage: kitten.r [-t TYPE] [-b] [-p] [-h] [-x] PACKAGE
 
--b --bunny	      install roxygen2 documentation example and roxygenize
--p --puppy	      invoke tinytest::puppy to set up testing
--h --help             show this help text
--x --usage            show help and short example usage"
+-t --type TYPE      type of kitten: plain, rcpp, arma, eigen. [default: plain]
+-b --bunny	    install roxygen2 documentation example and roxygenize
+-p --puppy	    invoke tinytest::puppy to set up testing
+-h --help           show this help text
+-x --usage          show help and short example usage"
 
 opt <- docopt(doc)			# docopt parsing
 
@@ -23,7 +24,12 @@ See http://dirk.eddelbuettel.com/code/littler.html for more information.\n")
 }
 
 ## maybe support path, author, maintainer, email, license, ...
-pkgKitten::kitten(opt$PACKAGE, puppy=opt$puppy, bunny=opt$bunny)
+ign <- switch(opt$type,
+              plain = pkgKitten::kitten(opt$PACKAGE, puppy=opt$puppy, bunny=opt$bunny),
+              rcpp = Rcpp::Rcpp.package.skeleton(opt$PACKAGE),
+              arma = RcppArmadillo::Rcpp.package.skeleton(opt$PACKAGE),
+              eigen = RcppEigen::Rcpp.package.skeleton(opt$PACKAGE))
+
 if (opt$puppy) {
     stopifnot(requireNamespace("tinytest", quietly=TRUE))
     tinytest::puppy(opt$PACKAGE)
