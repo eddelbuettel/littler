2a3fe94487f0c4250ad3d9104a8f70fd62f1cb3f
Dirk Eddelbuettel <edd@debian.org>
2021-09-14 17:11:33 -0500

    updated roxy.r

--- inst/examples/roxy.r
+++ inst/examples/roxy.r
@@ -10,11 +10,12 @@
 library(docopt)
 
 ## configuration for docopt
-doc <- "Usage: roxy.r [-n] [-f] [-h] [-x] [-r ROCLETS] [PACKAGES ...]
+doc <- "Usage: roxy.r [-n] [-f] [-m] [-h] [-x] [-r ROCLETS] [PACKAGES ...]
 
 -n --nocache          run the current version not the cached version
 -r --roclets ROCLETS  use roclets arguments for roxygenize [default: rd]
 -f --full             implies both '-n' and '-r NULL'
+-m --minimal          implies 'namespace,rd' roclets
 -h --help             show this help text
 -x --usage            show help and short example usage"
 opt <- docopt(doc)			# docopt parsing
@@ -27,8 +28,9 @@ Examples:
   roxy.r                     # update help pages for package, use cached version
   roxy.r -n                  # use non-cached version
   roxy.r -r NULL             # use full roclets i.e. collate,namespace,rd
-  roxy.r -r 'namespace,rd'   # use full roclets i.e. namespace,rd
+  roxy.r -r 'namespace,rd'   # use specified roclets i.e. namespace,rd
   roxy.r -f                  # use non-cached version and 'NULL' roclets
+  roxy.r -m                  # shorthand for use 'of namespace,rd' roclets
 
 roxy.r is part of littler which brings 'r' to the command-line. See the help for
 roxygenize for the different 'roclets' arguments; default value 'rd' means to only
@@ -40,6 +42,10 @@ See http://dirk.eddelbuettel.com/code/littler.html for more information.\n")
 if (opt$full) {
     opt$nocache <- TRUE
     opt$roclets <- "NULL"
+}
+if (opt$minimal) {
+    opt$nocache <- FALSE
+    opt$roclets <- "namespace,rd"
 }
 
 ## Works around the marvel that is version 6.1.0 or later
