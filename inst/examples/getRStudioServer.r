e243b88cee5fb73054ce14c9aad0083c568f3b79
Dirk Eddelbuettel <edd@debian.org>
2021-10-26 19:22:50 -0500

    update rstudio getters

--- inst/examples/getRStudioServer.r
+++ inst/examples/getRStudioServer.r
@@ -10,21 +10,25 @@
 #
 # todo: cmdline options for different download options
 
-suppressMessages(library(rvest))
+#suppressMessages(library(rvest))
 
 setwd("/tmp")                           # go to /tmp
 
 #url <- "https://dailies.rstudio.com/rstudioserver/oss/ubuntu/amd64/"
 #url <- "https://dailies.rstudio.com/rstudioserver/oss/ubuntu/x86_64/"
-url <- "https://dailies.rstudio.com/rstudioserver/oss/bionic/x86_64/"
-pg <- read_html(url(url))
-tb <- html_table(html_nodes(pg, "table"))[[1]]
-file <- tb[[1,1]]
+url <- "https://dailies.rstudio.com/rstudio/latest/index.json"
+#pg <- read_html(url(url))
+#tb <- html_table(html_nodes(pg, "table"))[[1]]
+js <- jsonlite::fromJSON(url)
+#file <- tb[[1,1]]
 #s3url <- "https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64/"
-s3url <- "https://s3.amazonaws.com/rstudio-ide-build/server%2Fbionic%2Famd64%2F"
+#s3url <- "https://s3.amazonaws.com/rstudio-ide-build/server%2Fbionic%2Famd64%2F"
 #fileurl <- paste0(s3url, file)
-fileurl <- paste0(s3url, gsub("\\+","%2B", file))
-cat("'", fileurl, "' -> '", file, "'\n", sep="")
+#fileurl <- paste0(s3url, gsub("\\+","%2B", file))
+#cat("'", fileurl, "' -> '", file, "'\n", sep="")
+fileurl <- js$products$server$platforms$bionic$link
+file <- basename(fileurl)
+cat("'", url, "' -> '", file, "'\n", sep="")
 download.file(fileurl, file) #, method="wget")
 
 ## possibly simpler
