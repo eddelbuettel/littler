8861985b9e3d12e52652a911e8c29a563f5568b2
Dirk Eddelbuettel <edd@debian.org>
2022-12-24 10:02:00 -0600

    Extend r2u.r with 'package' command, update docs

--- inst/examples/r2u.r
+++ inst/examples/r2u.r
@@ -8,7 +8,7 @@
 
 library(docopt)
 
-doc <- "Usage: r2u.r [--release DIST] [--debug] [--verbose] [--force] [--uncache] [--help] CMD [...]
+doc <- "Usage: r2u.r [--release DIST] [--debug] [--verbose] [--force] [--uncache] [--help] CMD ...
 
 Options:
 -r --release DIST   release distribution to use, one of 'focal' or 'jammy' [default: jammy]
@@ -20,8 +20,11 @@ Options:
 -h --help           show this help text
 
 Cmd:
-build        updates packages
-lastsync     reports most recent binary package sync
+build        updates all packages
+last         reports most recent binary package sync
+count        counts packages downloaded (locally) today
+table        tabulates packages downloaded today
+package      updates the package(s) named in ...
 
 Simple wrapper to 'r2u::buildUpdatedPackages(distro)'. The 'CMD' has to (for now)
 be 'build (for buildUpdatedPackages)'.
@@ -32,7 +35,12 @@ opt <- docopt(doc)
 if (!is.finite(match(opt$release, c("focal", "jammy"))))
     stop("Unknown distro '", opt$release, "'.", call. = FALSE)
 
-if (!is.finite(match(opt$CMD, c("build", "last", "count", "table"))))
+if (length(opt$CMD) > 1) {
+    opt$args <- opt$CMD[-1]
+    opt$CMD <- opt$CMD[1]
+}
+
+if (!is.finite(match(opt$CMD, c("build", "last", "count", "table", "package"))))
     stop("Unknown command '", opt$CMD, "'.", call. = FALSE)
 
 if (is.finite(match(opt$CMD, "build"))) {
@@ -48,6 +56,7 @@ if (is.finite(match(opt$CMD, "build"))) {
                          opt$verbose,
                          opt$force,
                          opt$xvfb)
+
 } else if (is.finite(match(opt$CMD, "last"))) {
     D <- RcppSimdJson::fload("https://packagemanager.rstudio.com/__api__/sources/1/transactions")
     ts <- anytime::utctime(D[1,"completed"])
@@ -55,13 +64,32 @@ if (is.finite(match(opt$CMD, "build"))) {
     un <- "days"
     if (dh <= 3) un <- "mins" else if (dh < 25) un <- "hours"
     cat("RSPM last updated", format(round(difftime(Sys.time(), ts, units=un),1)), "ago\n")
+
 } else if (is.finite(match(opt$CMD, "count"))) {
     ll <- readLines(pipe("bash -c ~/bin/web_who_what | grep '.*cranapt\\/pool\\/dists\\/.*\\/r-.*\\.deb$'"))
     cat(length(ll), ".deb files downloaded\n")
+
 } else if (is.finite(match(opt$CMD, "table"))) {
     con <- pipe("bash -c ~/bin/web_who_what | grep '.*cranapt\\/pool\\/dists\\/.*\\/r-.*\\.deb$'")
     ll <- readLines(con)
     close(con)
     tt <- table(gsub(".*r-(cran|bioc)-(.*)_\\d+.*", "\\2", ll, perl=TRUE))
     print(head(tt[order(-tt)], 10))
+
+} else if (is.finite(match(opt$CMD, "package"))) {
+    library(r2u)
+    if (opt$uncache) {
+        for (db in c(r2u:::.defaultCRANDBFile(), r2u:::.defaultAPFile()))
+            if (file.exists(db)) unlink(db)
+        r2u:::.loadDB()
+        r2u:::.loadAP()
+    }
+    for (p in opt$args) {
+        buildPackage(p,
+                     opt$release,
+                     opt$debug,
+                     opt$verbose,
+                     opt$force,
+                     opt$xvfb)
+    }
 }
