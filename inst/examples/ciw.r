a0915910acf534bd847aad21e9114aa0b304b701
Dirk Eddelbuettel <edd@debian.org>
2024-03-02 07:48:45 -0600

    Further tweaks to ciw.r

--- inst/examples/ciw.r
+++ inst/examples/ciw.r
@@ -11,24 +11,24 @@ if (!requireNamespace("ciw", quietly=TRUE))
 
 ## load docopt and cranincoming
 suppressMessages({
-    library(docopt)             # we need docopt (>= 0.3) as on CRAN
     library(ciw) 		        # at GitHub under eddelbuettel for now
+    library(docopt)             # we need docopt (>= 0.3) as on CRAN
 })
 
 ## configuration for docopt
-doc <- "Usage: ciw.r [-h] [-x] [-a] [-m] [-i] [-t] [-p] [-w] [-r] [-s] [-e] [-n] [-u] [ARG...]
+doc <- "Usage: ciw.r [-h] [-x] [-a] [-m] [-i] [-t] [-p] [-w] [-r] [-s] [-n] [-u] [-l rows] [ARG...]
 
--a --auto           use 'auto' mode of selected folders (see --usage)
 -m --mega           use 'mega' mode of all folders (see --usage)
 -i --inspect        visit 'inspect' folder
 -t --pretest        visit 'pretest' folder
 -p --pending        visit 'pending' folder
 -w --waiting        visit 'waiting' folder
 -r --recheck        visit 'waiting' folder
--e --archive        visit 'archive' folder
+-a --archive        visit 'archive' folder
 -n --newbies        visit 'newbies' folder
 -u --publish        visit 'publish' folder
 -s --skipsort       skip sorting of aggregate results by age
+-l --lines rows     print 'rows' rows of the data.table object [default: 50]
 -h --help           show this help text
 -x --usage          show help and short example usage"
 opt <- docopt(doc)			# docopt parsing
@@ -42,31 +42,33 @@ Examples:
   ciw.r -a                             # run with mode 'auto' resolved in incoming()
   ciw.r                                # run with defaults, same as '-itpwr'
 
-Currently, 'inspect', 'waiting', 'pending', 'pretest', 'recheck' are select as default.
+When no argument is given, 'auto' is selected which corresponds to 'inspect', 'waiting',
+'pending', 'pretest', and 'recheck'. Selecting '-m' or '--mega' are select as default.
 
-Folder selecting arguments are cumulative; 'auto' and 'mega' are single selections.
+Folder selecting arguments are cumulative; but 'mega' is a single selections of all folders
+(i.e. 'inspect', 'waiting', 'pending', 'pretest', 'recheck', 'archive', 'newbies', 'publish').
 
 ciw.r is part of littler which brings 'r' to the command-line.
 See https://dirk.eddelbuettel.com/code/littler.html for more information.\n")
     q("no")
 }
 
-args <- character()
-chk <- TRUE
+args <- character() 			# start with no arguments
 
 folders <-  c("inspect", "waiting", "pending", "pretest", "recheck", "archive", "newbies", "publish")
 
-for (dir in folders)
+for (dir in folders) 			# grow folder set as a select
     if (opt[[dir]])
         args <- c(args, dir)
 
-if (opt$auto) args <- "auto"
-if (opt$mega) args <- folders
+if (opt$mega) args <- folders	# or respect the nuclear option
+if (length(args) == 0)
+    args <- "auto"
 
-if (length(args) == 0) {
-    args <- c("inspect", "waiting", "pending", "pretest", "recheck")
-}
+chk <- length(args) <= 1   		# ask for argument check only on short argument vectoe
 
-res <- incoming(args, length(args) <= 1)
+res <- incoming(args, chk, !opt$skipsort)
 
-if (opt$skipsort) print(res[]) else print(res[order(Age)])
+options(datatable.print.nrows = as.numeric(opt$lines))
+
+print(res[])
