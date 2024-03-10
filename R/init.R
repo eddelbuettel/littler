8788d8d9a4a631ec51704fcfdb6024a83e8891df
Dirk Eddelbuettel <edd@debian.org>
2024-03-10 10:14:12 -0500

    Improved fix to fix

--- R/init.R
+++ R/init.R
@@ -5,12 +5,12 @@
     packageStartupMessage("and see 'vignette(\"littler-faq\") for some basic questions.")
     if (Sys.info()[["sysname"]] %in% c("Linux", "Darwin")) { # nocov start
         if (unname(Sys.which("r")) == "") {
-            loc <- system.file(if (nzchar(.Platform$r_arch)) file.path("bin", .Platform$r_arch) else "bin", "r", package="littler")
+            loc <- normalizePath(file.path("bin", .Platform$r_arch, "r", package="littler"))
             packageStartupMessage("You could link to the 'r' binary installed in\n'", loc, "'\n",
                                   "from '/usr/local/bin' in order to use 'r' for scripting.",
                                   "See the 'vignette(\"littler-faq\")' for more details.")
         if (Sys.info()[["sysname"]] == "Darwin")
-            packageStartupMessage("On OS X, 'r' and 'R' are the same so 'lr' is a possible alternate name for littler.")
+            packageStartupMessage("On maxOS, 'r' and 'R' are the same so 'lr' is a possible alternate name for littler.")
         } # nocov end
     }
 }
