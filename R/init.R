82b4ce199f8e0f5bc1f7d38da71f083b2314d60b
Berwin A Turlach <berwin.turlach@gmail.com>
2024-03-10 23:32:55 +0800

    Final fix to fix (?)

--- R/init.R
+++ R/init.R
@@ -5,7 +5,7 @@
     packageStartupMessage("and see 'vignette(\"littler-faq\") for some basic questions.")
     if (Sys.info()[["sysname"]] %in% c("Linux", "Darwin")) { # nocov start
         if (unname(Sys.which("r")) == "") {
-            loc <- normalizePath(file.path("bin", .Platform$r_arch, "r", package="littler"))
+            loc <- normalizePath(system.file("bin", .Platform$r_arch, "r", package="littler"))
             packageStartupMessage("You could link to the 'r' binary installed in\n'", loc, "'\n",
                                   "from '/usr/local/bin' in order to use 'r' for scripting.",
                                   "See the 'vignette(\"littler-faq\")' for more details.")
