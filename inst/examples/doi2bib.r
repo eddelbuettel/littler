5b821a9f78f5ef5f5891145528b28b098fc04467
Dirk Eddelbuettel <edd@debian.org>
2024-03-14 07:55:46 -0500

    Enhancements to doi2bib.r

--- inst/examples/doi2bib.r
+++ inst/examples/doi2bib.r
@@ -10,10 +10,14 @@
 
 ## load docopt package from CRAN
 library(docopt)
+
+if (!requireNamespace("httr", quietly=TRUE))
+    stop("Please install 'httr' from CRAN.", call. = FALSE)
+
 library(httr)
 
 ## configuration for docopt
-doc <- "Usage: doi2bib.r [-h] [-x] DOI
+doc <- "Usage: doi2bib.r [-h] [-x] DOI...
 
 -h --help             show this help text
 -x --usage            show help and short example usage"
@@ -23,13 +27,15 @@ opt <- docopt(doc)          # docopt parsing
 if (opt$usage) {
     cat(doc, "\n\n")
     cat("Examples:
-  doi2bib..r 10.1002/evan.10110
+  doi2bib.r 10.1002/evan.10110 10.1007/978-1-4614-6868-4
 
 doi2bib.r is part of littler which brings 'r' to the command-line.
 See http://dirk.eddelbuettel.com/code/littler.html for more information.\n")
     q("no")
 }
 
-res <- GET(paste0("https://doi.org/", opt$DOI),
-           add_headers(Accept="application/x-bibtex"))
-cat(rawToChar(res$content))
+for (arg in opt$DOI) {
+    res <- GET(paste0("https://doi.org/", arg),
+               add_headers(Accept="application/x-bibtex"))
+    cat(rawToChar(res$content))
+}
