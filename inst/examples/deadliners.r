45b7d96e42febd133001365861ae2e330ac3d732
Dirk Eddelbuettel <edd@debian.org>
2024-06-24 10:21:38 -0500

    Enhance deadliners a little

--- inst/examples/deadliners.r
+++ inst/examples/deadliners.r
@@ -6,7 +6,24 @@
 #
 # Released under GPL (>= 2)
 
+## borrowed with love from tinytest and marginally extended
+.color_str <- function(x, color){
+    cmap <- c(cyan=36, red=31, green=32, blue=34, purple=35)
+    sprintf("\033[0;%dm%s\033[0m", cmap[color], x)
+}
+.blue <- function(txt) .color_str(txt, "blue")
+.purple <- function(txt) .color_str(txt, "purple")
+
 db <- as.data.frame(tools::CRAN_package_db())
 dd <- with(db, db[!is.na(Deadline), c("Package", "Version", "Deadline")])
 dd <- with(dd, dd[order(Deadline, Package), ])
-print(dd, row.names=FALSE)
+
+cols <- if (nzchar(Sys.which("tput"))) as.integer(system("tput cols", intern=TRUE)) else 80L
+dts <- unique(dd[, "Deadline"])
+for (d in seq_along(dts)) {
+    cat(.blue(dts[d]), ": ", sep="")
+    ind <- with(dd, Deadline == dts[d])
+    blk <- dd[ind, ]
+    pv <- paste0(.purple(blk[,1]), " (", blk[,2], ")")
+    cat(paste(strwrap(paste(pv, collapse=", "), cols), "\n"))
+}
